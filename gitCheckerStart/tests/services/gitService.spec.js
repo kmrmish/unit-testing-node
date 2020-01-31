571c8b23345bac8e7609df1cdb15cc4e8b3e0ded
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-01-31 19:46:20 +0530

    GitService, getUser, should return user and repos

---
+++ gitCheckerStart/tests/services/gitService.spec.js
@@ -0,0 +1,20 @@
+var chai = require('chai'), 
+    sinon = require('sinon');
+
+chai.should();
+
+var gitService = require('../../services/gitService')();
+
+describe('GitService', function(){
+    describe('GetUser', function(){
+        it('should return user and repos', function(){
+            this.timeout(10000);
+            return gitService.getUser('jonathanfmills').then(
+                function(user) {
+                    user.login.should.equal('jonathanfmills');
+                    user.should.have.property('repos');
+                }
+            );
+        });
+    });
+});
