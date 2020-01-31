a134c7dbd5cee7fe418fc9ba1a9cbbdd0787dc5d
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-01-31 11:33:03 +0530

    before each

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,21 @@
 function AuthController(){
 
-        function isAuthorized(roles, neededRole){
-            return roles.indexOf(neededRole) >= 0;
-        }
-        return {isAuthorized};
+    var roles;
+    function setRoles(role){
+        roles = role;
+    }
+
+    function isAuthorized(neededRole){
+        return roles.indexOf(neededRole) >= 0;
+    }
+
+    function isAuthorizedAsync(neededRole, cb){
+        setTimeout(function(){
+            cb(roles.indexOf(neededRole) >= 0);
+        }, 0); //default timeout for mocha test is 2000 miliseconds
+    }
+
+    return {isAuthorized, isAuthorizedAsync, setRoles};
 }
 
 module.exports = AuthController();
--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,14 +1,39 @@
 var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
 
+
 describe('AuthController',function(){
+    
+    beforeEach('setting up roles', function settingUpRoles(){
+        authController.setRoles(['user']);
+    });
+
     describe('isAuthrized',function(){
         it('Should return false if not authorized', function(){
-            assert.equal(false, authController.isAuthorized(['user'], 'admin'));
+            assert.equal(false, authController.isAuthorized('admin'));
         });
 
-        it('Should return false if not authorized', function(){
-            assert.equal(true, authController.isAuthorized(['user'], 'admin'));
+        it('Should return true if authorized', function(){
+            authController.setRoles(['user','admin']);
+            assert.equal(true, authController.isAuthorized('admin'));
+        });
+    });
+
+    describe('isAuthrizedAsync',function(){
+        
+
+        it('Should return false if not authorized', function(done){
+
+            /**@description:  
+             * this will now work if we use arrow functions, 
+             * mocha suggests not to use arrow functions
+             * */ 
+            // this.timeout(2500);
+            
+            authController.isAuthorizedAsync('admin',function(isAuth){
+                assert.equal(false, isAuth);
+                done();
+            });
         });
     });
 });
