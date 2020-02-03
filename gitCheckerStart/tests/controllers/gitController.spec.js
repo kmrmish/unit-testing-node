bb43ae21552c738f22aecf79f458ae321f766f5d
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-02-03 15:53:41 +0530

    rewire and testing controllers

---
+++ gitCheckerStart/tests/controllers/gitController.spec.js
@@ -0,0 +1,29 @@
+var rewire = require('rewire');
+var GitCtrl = rewire('../../controllers/gitController');
+var gitController = GitCtrl();
+var chai = require('chai');
+var sinon = require('sinon');
+chai.should();
+
+var getUser;
+describe('gitController',function(){
+    beforeEach(function(){
+        var gitService = GitCtrl.__get__('gitService');
+        getUser = sinon.spy(gitService,'getUser');
+        GitCtrl.__set__('gitService', gitService);
+    });
+    it.only('should get user and repos from git service', function(done){
+        // this.timeout(10000);
+        var req = {params: {userId: 'kmrmish'}};
+        var res = {json: test}
+
+        function test(user) {
+            getUser.getCall(0).args[0].should.equal('kmrmish');
+            getUser.calledOnce.should.be.true;
+            user.login.should.equal('kmrmish');
+            done();
+        }
+        gitController.userGet(req,res);
+
+    });
+});
